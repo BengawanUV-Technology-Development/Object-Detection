1b9593dd17e429374f0c025d3c0c1161057071df
NaufalKusumo <naufalkusumo@student.uns.ac.id>
2026-01-17 23:03:09 +0700

    Update train.py

--- train/train.py
+++ train/train.py
@@ -1,3 +1,11 @@
 from ultralytics import YOLO
 import torch
 
+if __name__ == '__main__':
+    print(torch.__version__)
+    print(torch.version.cuda)
+    if torch.cuda.is_available():
+        print(torch.cuda.get_device_name(0))
+        print("hellloooo")
+    else:
+        print("akdfjalkjfdl")
